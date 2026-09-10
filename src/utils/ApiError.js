9e36f3a4aa44e502fb001aeb49deaba0b273b5a7
satwik <rankurstk5678@gmail.com>
2026-09-10 16:20:09 +0530

    .

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -21,3 +21,14 @@ class ApiError extends Error {
 }
 
 export { ApiError };
+
+// PORT=8000
+// MONGO_URI=mongodb+srv://satwik:.lly3ya1.mongodb.net
+// CORS_ORIGIN=*
+// ACCESS_TOKEN_SECRET=Hds0PvZAV9dHiHM9P2SSqbfRugfgHXMx6rkvwpTYmkKA1cBoZq
+// ACCESS_TOKEN_EXPIRY=1d
+// REFRESH_TOKEN_SECRET=shsGA0tJzzIPP3a8bYBSzOZjY4xEpom2bTtQujYsqKcfcI4J7q
+// REFRESH_TOKEN_EXPIRY=10d
+// CLOUDINARY_CLOUD_NAME=pqcrk
+// CLOUDINARY_API_KEY=11794899277
+// CLOUDINARY_API_SECRET=YIbZ7S8FmEv4yjmlJMWHch
