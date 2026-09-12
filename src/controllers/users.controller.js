6bd130ef5de1cc7d2131c44837f25d99e29a08cc
satwik <rankurstk5678@gmail.com>
2026-09-12 13:40:50 +0530

    add change password functionality and update routes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -183,9 +183,25 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+  const user = await User.findById(req.user._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(currentPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Current password is incorrect");
+  }
+  user.password = newPassword;
+  await user.save({validateBeforeSave: false});
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
 export { 
   registerUser, 
   loginUser, 
   logoutUser, 
-  refreshAccessToken 
+  refreshAccessToken,
+  changeCurrentPassword
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { refreshAccessToken, registerUser } from "../controllers/users.controller.js";
+import { 
+  refreshAccessToken, 
+  registerUser, 
+  changeCurrentPassword, 
+  loginUser, 
+  logoutUser 
+} from "../controllers/users.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
-import { loginUser } from "../controllers/users.controller.js";
-import {logoutUser} from "../controllers/users.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -20,5 +24,6 @@ router.route("/login").post(loginUser);
 //secure routes
 router.route("/logout").post(authMiddleware,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(authMiddleware, changeCurrentPassword);
 
 export default router;
